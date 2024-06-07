bfb80ec3e6b28db99ac9a61ddfe5b476901b42bf
Rory Garand <github@rorygarand.com>
2024-06-07 00:16:32 -0400

    comment

--- src/games/rangeDice/index.ts
+++ src/games/rangeDice/index.ts
@@ -1,172 +1,172 @@
-import { SuiClient, SuiTransactionBlockResponse } from "@mysten/sui.js/client";
-import {
-    TransactionArgument,
-    TransactionBlock as TransactionBlockType,
-    TransactionObjectArgument
-} from "@mysten/sui.js/transactions";
+// import { SuiClient, SuiTransactionBlockResponse } from "@mysten/sui.js/client";
+// import {
+//     TransactionArgument,
+//     TransactionBlock as TransactionBlockType,
+//     TransactionObjectArgument
+// } from "@mysten/sui.js/transactions";
 
-import { nanoid } from 'nanoid';
+// import { nanoid } from 'nanoid';
 
-import {
-    BLS_SETTLER_MODULE_NAME,
-    BLS_VERIFIER_OBJ,
-    DICE_CORE_PACKAGE_ID,
-    DICE_MODULE_NAME,
-    DICE_STRUCT_NAME,
-    UNI_HOUSE_OBJ,
-    UNIHOUSE_CORE_PACKAGE
-} from "../../constants";
-import { getBlsGameInfos, sleep } from "../../utils";
+// import {
+//     BLS_SETTLER_MODULE_NAME,
+//     BLS_VERIFIER_OBJ,
+//     DICE_CORE_PACKAGE_ID,
+//     DICE_MODULE_NAME,
+//     DICE_STRUCT_NAME,
+//     UNI_HOUSE_OBJ,
+//     UNIHOUSE_CORE_PACKAGE
+// } from "../../constants";
+// import { getBlsGameInfos, sleep } from "../../utils";
 
-// Note: 0 - 5 for dice rolls, and 6 = even, 7 = odd, 8 = small, 9 = big
-type BetType = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9;
+// // Note: 0 - 5 for dice rolls, and 6 = even, 7 = odd, 8 = small, 9 = big
+// type BetType = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9;
 
-type RangeDiceResult = any;
+// type RangeDiceResult = any;
 
-export interface DiceInput {
-    betType: BetType;
-    coin: TransactionObjectArgument;
-    coinType: string;
-    transactionBlock: TransactionBlockType;
-}
+// export interface DiceInput {
+//     betType: BetType;
+//     coin: TransactionObjectArgument;
+//     coinType: string;
+//     transactionBlock: TransactionBlockType;
+// }
 
-interface InternalDiceInput extends DiceInput {
-    dicePackageId: string;
-}
+// interface InternalDiceInput extends DiceInput {
+//     dicePackageId: string;
+// }
 
-export interface DiceResultInput {
-    betType: BetType;
-    coinType: string;
-    gameSeed: string;
-    pollInterval?: number;
-    transactionResult: SuiTransactionBlockResponse;
-}
+// export interface DiceResultInput {
+//     betType: BetType;
+//     coinType: string;
+//     gameSeed: string;
+//     pollInterval?: number;
+//     transactionResult: SuiTransactionBlockResponse;
+// }
 
-interface InternalDiceResultInput extends DiceResultInput {
-    diceCorePackageId: string;
-    suiClient: SuiClient;
-}
+// interface InternalDiceResultInput extends DiceResultInput {
+//     diceCorePackageId: string;
+//     suiClient: SuiClient;
+// }
 
-export interface DiceResponse {
-    ok: boolean;
-    err?: Error;
-    gameSeed?: string;
-    receipt?: TransactionArgument;
-}
+// export interface DiceResponse {
+//     ok: boolean;
+//     err?: Error;
+//     gameSeed?: string;
+//     receipt?: TransactionArgument;
+// }
 
-export interface DiceResultResponse {
-    ok: boolean;
-    err?: Error;
-    results?: DiceResult[];
-}
+// export interface DiceResultResponse {
+//     ok: boolean;
+//     err?: Error;
+//     results?: DiceResult[];
+// }
 
-// Add dice to the transaction block
-export const createDice = ({
-    betType,
-    coin,
-    coinType,
-    dicePackageId,
-    transactionBlock
-}: InternalDiceInput): DiceResponse => {
-    const res: DiceResponse = { ok: true };
+// // Add dice to the transaction block
+// export const createDice = ({
+//     betType,
+//     coin,
+//     coinType,
+//     dicePackageId,
+//     transactionBlock
+// }: InternalDiceInput): DiceResponse => {
+//     const res: DiceResponse = { ok: true };
 
-    try {
-        // This adds some extra entropy to the dice itself
-        const userRandomness = Buffer.from(nanoid(512), 'utf8');
+//     try {
+//         // This adds some extra entropy to the dice itself
+//         const userRandomness = Buffer.from(nanoid(512), 'utf8');
 
-        const [receipt] = transactionBlock.moveCall({
-            target: `${dicePackageId}::${DICE_MODULE_NAME}::start_game`,
-            typeArguments: [coinType],
-            arguments: [
-                transactionBlock.object(UNI_HOUSE_OBJ),
-                transactionBlock.object(BLS_VERIFIER_OBJ),
-                transactionBlock.pure(Array.from(userRandomness), "vector<u8>"),
-                transactionBlock.pure(betType),
-                coin,
-            ],    
-        });
+//         const [receipt] = transactionBlock.moveCall({
+//             target: `${dicePackageId}::${DICE_MODULE_NAME}::start_game`,
+//             typeArguments: [coinType],
+//             arguments: [
+//                 transactionBlock.object(UNI_HOUSE_OBJ),
+//                 transactionBlock.object(BLS_VERIFIER_OBJ),
+//                 transactionBlock.pure(Array.from(userRandomness), "vector<u8>"),
+//                 transactionBlock.pure(betType),
+//                 coin,
+//             ],    
+//         });
 
-        res.gameSeed = Buffer.from(userRandomness).toString("hex");
-        res.receipt = receipt;
-    } catch (err) {
-        res.ok = false;
-        res.err = err;
-    }
+//         res.gameSeed = Buffer.from(userRandomness).toString("hex");
+//         res.receipt = receipt;
+//     } catch (err) {
+//         res.ok = false;
+//         res.err = err;
+//     }
 
-    return res;
-};
+//     return res;
+// };
 
-export const getDiceResult = async ({
-    betType,
-    coinType,
-    diceCorePackageId,
-    gameSeed,
-    pollInterval = 3000,
-    suiClient,
-    transactionResult
-}: InternalDiceResultInput): Promise<DiceResultResponse> => {
-    const res: DiceResultResponse = { ok: true };
+// export const getDiceResult = async ({
+//     betType,
+//     coinType,
+//     diceCorePackageId,
+//     gameSeed,
+//     pollInterval = 3000,
+//     suiClient,
+//     transactionResult
+// }: InternalDiceResultInput): Promise<DiceResultResponse> => {
+//     const res: DiceResultResponse = { ok: true };
 
-    try {
-        const gameInfos = getBlsGameInfos({
-            coinType,
-            corePackageId: DICE_CORE_PACKAGE_ID,
-            gameSeed,
-            moduleName: DICE_MODULE_NAME,
-            structName: DICE_STRUCT_NAME,
-            transactionResult
-        });
+//     try {
+//         const gameInfos = getBlsGameInfos({
+//             coinType,
+//             corePackageId: DICE_CORE_PACKAGE_ID,
+//             gameSeed,
+//             moduleName: DICE_MODULE_NAME,
+//             structName: DICE_STRUCT_NAME,
+//             transactionResult
+//         });
 
-        const results = [];
+//         const results = [];
 
-        while (results.length === 0) {
-            try {
-                const events = await suiClient.queryEvents({
-                    query: {
-                        MoveEventType: `${UNIHOUSE_CORE_PACKAGE}::${BLS_SETTLER_MODULE_NAME}::SettlementEvent<${coinType}, ${diceCorePackageId}::${DICE_MODULE_NAME}::${DICE_STRUCT_NAME}>`
-                    },
-                    limit: 50,
-                    order: 'descending'
-                });
+//         while (results.length === 0) {
+//             try {
+//                 const events = await suiClient.queryEvents({
+//                     query: {
+//                         MoveEventType: `${UNIHOUSE_CORE_PACKAGE}::${BLS_SETTLER_MODULE_NAME}::SettlementEvent<${coinType}, ${diceCorePackageId}::${DICE_MODULE_NAME}::${DICE_STRUCT_NAME}>`
+//                     },
+//                     limit: 50,
+//                     order: 'descending'
+//                 });
 
-                events.data.map(event => {
-                    console.log(event)
-                    console.log(event.parsedJson)
-                })
+//                 events.data.map(event => {
+//                     console.log(event)
+//                     console.log(event.parsedJson)
+//                 })
 
-                // results = events.data.reduce((acc, current) => {
-                //     const {
-                //         bet_id,
-                //         settlements
-                //     } = current.parsedJson as CoinflipParsedJson;
+//                 // results = events.data.reduce((acc, current) => {
+//                 //     const {
+//                 //         bet_id,
+//                 //         settlements
+//                 //     } = current.parsedJson as CoinflipParsedJson;
 
-                //     if (bet_id == gameInfos[0].gameId) {
-                //         const { player_won } = settlements[0];
+//                 //     if (bet_id == gameInfos[0].gameId) {
+//                 //         const { player_won } = settlements[0];
 
-                //         if (player_won) {
-                //             acc.push(betType === 0 ? 0 : 1);
-                //         } else {
-                //             acc.push(betType === 0 ? 1 : 0);
-                //         }
-                //     }
+//                 //         if (player_won) {
+//                 //             acc.push(betType === 0 ? 0 : 1);
+//                 //         } else {
+//                 //             acc.push(betType === 0 ? 1 : 0);
+//                 //         }
+//                 //     }
 
-                //     return acc;
-                // }, []);
-            } catch (err) {
-                console.error(`DOUBLEUP - Error querying events: ${err}`);
-            }
+//                 //     return acc;
+//                 // }, []);
+//             } catch (err) {
+//                 console.error(`DOUBLEUP - Error querying events: ${err}`);
+//             }
 
-            if (results.length === 0) {
-                console.log(`DOUBLEUP - No results found. Trying again in ${pollInterval / 1000} seconds.`);
-                await sleep(pollInterval);
-            }
-        }
+//             if (results.length === 0) {
+//                 console.log(`DOUBLEUP - No results found. Trying again in ${pollInterval / 1000} seconds.`);
+//                 await sleep(pollInterval);
+//             }
+//         }
 
-        res.results = results;
-    } catch (err) {
-        res.ok = false;
-        res.err = err;
-    }
+//         res.results = results;
+//     } catch (err) {
+//         res.ok = false;
+//         res.err = err;
+//     }
 
-    return res;
-};
+//     return res;
+// };
