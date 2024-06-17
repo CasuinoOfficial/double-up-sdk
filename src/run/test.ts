3c29d2f7bd85e42b432c2a550a9102a634cda92e
Rory Garand <github@rorygarand.com>
2024-06-17 16:17:43 -0400

    range dice tests

--- src/run/test.ts
+++ src/run/test.ts
@@ -13,12 +13,17 @@ const suiKit = new SuiKit({ mnemonics: MNEMONICS });
 // const DESUI_PLINKO_CORE_PACKAGE_ID = "0xe73647314c4d0d007d3e65c9eb0c609104a4d03a0743b4b7177752bcb1586ac3";
 // const DESUI_PLINKO_VERIFIER_ID = "0x85fed939bc09d61a314a9c0d4d16370be788a538f351b82b6b3db1ae4f1c7374";
 
+const DESUI_RANGE_DICE_PACKAGE_ID = "0x7a05d26f35fee4e6ab9d59cb6f7f48e90cefe2c0742e304b555fa3be8dcf2cea";
+const DESUI_RANGE_DICE_CORE_PACKAGE_ID = "0x7a05d26f35fee4e6ab9d59cb6f7f48e90cefe2c0742e304b555fa3be8dcf2cea";
+
 const dbClient = new DoubleUpClient({
     // limboCorePackageId: DESUI_LIMBO_CORE_PACKAGE_ID,
     // limboPackageId: DESUI_LIMBO_PACKAGE_ID,
     // plinkoCorePackageId: DESUI_PLINKO_CORE_PACKAGE_ID,
     // plinkoPackageId: DESUI_PLINKO_PACKAGE_ID,
     // plinkoVerifierId: DESUI_PLINKO_VERIFIER_ID,
+    rangeDicePackageId: DESUI_RANGE_DICE_PACKAGE_ID,
+    rangeDiceCorePackageId: DESUI_RANGE_DICE_CORE_PACKAGE_ID,
 
     suiClient: suiKit.client()
 });
@@ -84,7 +89,7 @@ const testCoinflip = async () => {
 };
 
 const testDice = async () => {
-    // odd
+    // even
     const betType = 6;
     const betAmount = 500000000;
 
@@ -258,6 +263,116 @@ const testPlinko = async () => {
     }
 };
 
+const testRangeDiceOverUnder = async () => {
+    // over
+    const betType = 0;
+    const betAmount = 500000000;
+
+    const range = 1;
+
+    const coinType = "0x2::sui::SUI";
+
+    try {
+        const txb = new SuiTxBlock();
+
+        const [coin] = txb.splitCoins(txb.gas, [txb.pure(betAmount, "u64")]);
+
+        const { ok: gameOk, err: gameErr, gameSeed } = dbClient.createRangeDice({
+            betType,
+            coin,
+            coinType,
+            range,
+            transactionBlock: txb.txBlock
+        });
+
+        console.log("Added ranged dice (over/under) to transaction block.");
+
+        if (!gameOk) {
+            throw gameErr;
+        }
+
+        const transactionResult = await suiKit.signAndSendTxn(txb);
+
+        if (transactionResult.effects.status.status === 'failure') {
+            throw new Error(transactionResult.effects.status.error);
+        }
+
+        console.log("Signed and sent transaction.");
+        console.log(transactionResult);
+
+        const { ok: resultsOk, err: resultsErr, results } = await dbClient.getRangeDiceResult({
+            betType,
+            coinType,
+            gameSeed,
+            transactionResult
+        });
+
+        if (!resultsOk) {
+            throw resultsErr;
+        }
+
+        console.log("Retrieved dice results.");
+        console.log(results);
+    } catch (err) {
+        console.log(err);
+    }
+};
+
+const testRangeDiceInsideOutside = async () => {
+    // inside
+    const betType = 2;
+    const betAmount = 500000000;
+
+    const range = [3, 4];
+
+    const coinType = "0x2::sui::SUI";
+
+    try {
+        const txb = new SuiTxBlock();
+
+        const [coin] = txb.splitCoins(txb.gas, [txb.pure(betAmount, "u64")]);
+
+        const { ok: gameOk, err: gameErr, gameSeed } = dbClient.createRangeDice({
+            betType,
+            coin,
+            coinType,
+            range,
+            transactionBlock: txb.txBlock
+        });
+
+        console.log("Added ranged dice (inside/out) to transaction block.");
+
+        if (!gameOk) {
+            throw gameErr;
+        }
+
+        const transactionResult = await suiKit.signAndSendTxn(txb);
+
+        if (transactionResult.effects.status.status === 'failure') {
+            throw new Error(transactionResult.effects.status.error);
+        }
+
+        console.log("Signed and sent transaction.");
+        console.log(transactionResult);
+
+        const { ok: resultsOk, err: resultsErr, results } = await dbClient.getRangeDiceResult({
+            betType,
+            coinType,
+            gameSeed,
+            transactionResult
+        });
+
+        if (!resultsOk) {
+            throw resultsErr;
+        }
+
+        console.log("Retrieved dice results.");
+        console.log(results);
+    } catch (err) {
+        console.log(err);
+    }
+};
+
 const testRPS = async () => {
     // rock
     const betType = 0;
@@ -327,8 +442,14 @@ const testRPS = async () => {
             case 'dice':
                 testDice();
                 break;
+            case 'insideoutside':
+                testRangeDiceInsideOutside();
+                break;
             case 'limbo':
                 testLimbo();
+                break;
+            case 'overunder':
+                testRangeDiceOverUnder();
                 break;
             case 'plinko':
                 testPlinko();
