c768076904a552ee7a00052d3942cb2ba73a4451
Jonathan Kim <kim.jonathan426@gmail.com>
2024-10-01 16:52:49 +0700

    rps converted to play

--- src/games/rps/index.ts
+++ src/games/rps/index.ts
@@ -20,19 +20,23 @@ import {
   UNI_HOUSE_OBJ_ID,
 } from "../../constants/mainnetConstants";
 import { KioskClient } from "@mysten/kiosk";
-import { getAssetIndex, getTypesFromVoucher, getVoucherBank } from "../../utils";
+import {
+  getAssetIndex,
+  getTypesFromVoucher,
+  getVoucherBank,
+} from "../../utils";
 
 // 0: Rock, 1: Paper, 2: Scissors
 export type BetType = 0 | 1 | 2;
 
 export interface RPSInput {
   betTypes: Array<BetType>;
-  coin: TransactionObjectArgument; 
+  coin: TransactionObjectArgument;
   coinType: string;
   partnerNftType?: string;
   nfts?: TransactionArgument;
   kioskId?: string;
-  kioskOwnerCap?: string; 
+  kioskOwnerCap?: string;
   origin?: string;
   transaction: TransactionType;
 }
@@ -122,9 +126,8 @@ export const createRockPaperScissors = ({
   partnerNftListId,
   rpsPackageId,
   transaction,
-  origin
+  origin,
 }: InternalRPSInput) => {
-  let assetIndex = getAssetIndex(coinType);
   transaction.setGasBudget(100_000_000);
 
   if (
@@ -132,85 +135,33 @@ export const createRockPaperScissors = ({
     typeof partnerNftType === "string" &&
     nfts !== undefined
   ) {
-
-    if (coinType in SUILEND_ASSET_LIST) {
-      transaction.moveCall({
-        target: `${rpsPackageId}::${RPS_MODULE_NAME}::play_with_partner_0`,
-        typeArguments: [coinType, partnerNftType],
-        arguments: [
-          transaction.object(UNI_HOUSE_OBJ_ID),
-          transaction.object(RAND_OBJ_ID),
-          transaction.pure(
-            bcs.vector(bcs.U64).serialize(betTypes)
-          ),
-          coin,
-          transaction.object(partnerNftListId),
-          transaction.object(kioskId),
-          transaction.object(kioskOwnerCap),
-          nfts,
-          transaction.pure.string(origin ?? "DoubleUp"),
-          transaction.object(SUILEND_POND_SUI_POOL_OBJ_ID),
-          transaction.object(SUILEND_MARKET),
-          transaction.object(CLOCK_OBJ_ID),
-          transaction.object(PYTH_SUI_PRICE_INFO_OBJ_ID),
-          transaction.pure.u64(assetIndex),
-        ],
-      });
-    } else {
-      transaction.moveCall({
-        target: `${rpsPackageId}::${RPS_MODULE_NAME}::play_with_partner`,
-        typeArguments: [coinType, partnerNftType],
-        arguments: [
-          transaction.object(UNI_HOUSE_OBJ_ID),
-          transaction.object(RAND_OBJ_ID),
-          transaction.pure(
-            bcs.vector(bcs.U64).serialize(betTypes)
-          ),
-          coin,
-          transaction.object(partnerNftListId),
-          transaction.object(kioskId),
-          transaction.object(kioskOwnerCap),
-          nfts,
-          transaction.pure.string(origin ?? "DoubleUp"),
-        ],
-      });
-    };
+    transaction.moveCall({
+      target: `${rpsPackageId}::${RPS_MODULE_NAME}::play_with_partner`,
+      typeArguments: [coinType, partnerNftType],
+      arguments: [
+        transaction.object(UNI_HOUSE_OBJ_ID),
+        transaction.object(RAND_OBJ_ID),
+        transaction.pure(bcs.vector(bcs.U64).serialize(betTypes)),
+        coin,
+        transaction.object(partnerNftListId),
+        transaction.object(kioskId),
+        transaction.object(kioskOwnerCap),
+        nfts,
+        transaction.pure.string(origin ?? "DoubleUp"),
+      ],
+    });
   } else {
-
-    if (coinType in SUILEND_ASSET_LIST) {
-      transaction.moveCall({
-        target: `${rpsPackageId}::${RPS_MODULE_NAME}::play_0`,
-        typeArguments: [coinType],
-        arguments: [
-          transaction.object(UNI_HOUSE_OBJ_ID),
-          transaction.object(RAND_OBJ_ID),
-          transaction.pure(
-            bcs.vector(bcs.U64).serialize(betTypes)
-          ),
-          coin,
-          transaction.pure.string(origin ?? "DoubleUp"),
-          transaction.object(SUILEND_POND_SUI_POOL_OBJ_ID),
-          transaction.object(SUILEND_MARKET),
-          transaction.object(CLOCK_OBJ_ID),
-          transaction.object(PYTH_SUI_PRICE_INFO_OBJ_ID),
-          transaction.pure.u64(assetIndex),
-        ],
-      });
-    } else {
-      transaction.moveCall({
-        target: `${rpsPackageId}::${RPS_MODULE_NAME}::play`,
-        typeArguments: [coinType],
-        arguments: [
-          transaction.object(UNI_HOUSE_OBJ_ID),
-          transaction.object(RAND_OBJ_ID),
-          transaction.pure(
-            bcs.vector(bcs.U64).serialize(betTypes)
-          ),
-          coin,
-          transaction.pure.string(origin ?? "DoubleUp"),
-        ],
-      });
-    }
+    transaction.moveCall({
+      target: `${rpsPackageId}::${RPS_MODULE_NAME}::play`,
+      typeArguments: [coinType],
+      arguments: [
+        transaction.object(UNI_HOUSE_OBJ_ID),
+        transaction.object(RAND_OBJ_ID),
+        transaction.pure(bcs.vector(bcs.U64).serialize(betTypes)),
+        coin,
+        transaction.pure.string(origin ?? "DoubleUp"),
+      ],
+    });
   }
 };
 
@@ -221,31 +172,24 @@ export const createRockPaperScissorsWithVoucher = async ({
   client,
   rpsPackageId,
   transaction,
-  origin
+  origin,
 }: InternalRPSVoucherInput) => {
   try {
     let [coinType, voucherType] = await getTypesFromVoucher(voucherId, client);
-    let assetIndex = getAssetIndex(coinType);
     let voucherBank = getVoucherBank(coinType);
     transaction.setGasBudget(20_000_000);
+
     transaction.moveCall({
-      target: `${rpsPackageId}::${RPS_MODULE_NAME}::play_with_voucher_0`,
+      target: `${rpsPackageId}::${RPS_MODULE_NAME}::play_with_voucher`,
       typeArguments: [coinType, voucherType],
       arguments: [
         transaction.object(UNI_HOUSE_OBJ_ID),
         transaction.object(RAND_OBJ_ID),
-        transaction.pure(
-          bcs.vector(bcs.U64).serialize(betTypes)
-        ),
+        transaction.pure(bcs.vector(bcs.U64).serialize(betTypes)),
         transaction.pure.u64(betSize),
         transaction.object(voucherId),
         transaction.object(voucherBank),
         transaction.pure.string(origin ?? "DoubleUp"),
-        transaction.object(SUILEND_POND_SUI_POOL_OBJ_ID),
-        transaction.object(SUILEND_MARKET),
-        transaction.object(CLOCK_OBJ_ID),
-        transaction.object(PYTH_SUI_PRICE_INFO_OBJ_ID),
-        transaction.pure.u64(assetIndex),
       ],
     });
   } catch (e) {
