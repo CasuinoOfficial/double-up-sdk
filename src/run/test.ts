d415e9c2ab97ece57ac737667604ff84622e1bd3
Dan Hsu <hsu86110@gmail.com>
2024-12-05 19:59:26 +0900

    feature: modified test code

--- src/run/test.ts
+++ src/run/test.ts
@@ -366,7 +366,7 @@ const dbClient = new DoubleUpClient({
           client,
           keypair,
           "0x2::sui::SUI", //coinType
-          "", //objectId
+          "", //objectType
           "0x421c1b4dc2022b14e7905bb57d555651617111bb9947c19563b825eeee962f1a", // gachaponId
           "0x904862e7054e948c89d6b2a3971e182af88542925f034d2e872357660a4f51e5" // keeperCapId
         );
@@ -378,7 +378,7 @@ const dbClient = new DoubleUpClient({
           client,
           keypair,
           "0x2::sui::SUI", //coinType
-          "", //objectId
+          "", //objectType
           "0x421c1b4dc2022b14e7905bb57d555651617111bb9947c19563b825eeee962f1a", // gachaponId
           "0x904862e7054e948c89d6b2a3971e182af88542925f034d2e872357660a4f51e5" // keeperCapId
         );
@@ -390,7 +390,7 @@ const dbClient = new DoubleUpClient({
           keypair,
           "0x2::sui::SUI", //coinType
           "0x421c1b4dc2022b14e7905bb57d555651617111bb9947c19563b825eeee962f1a", // gachaponId
-          "", //objectId
+          "", //objectType
           "" //recipient
         );
         break;
